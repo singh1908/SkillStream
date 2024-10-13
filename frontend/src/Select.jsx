b6cfb1ac5e5a5ba1dff07b4e934b4b379bdac271
Vivek Kumar Singh <vivekkumarsingh.mail04@gmail.com>
2024-10-13 13:04:26 +0530

    image added

--- frontend/src/Select.jsx
+++ frontend/src/Select.jsx
@@ -1,21 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import welcome from "./assets/welcome.jpg"
 
 const Select = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="w-full max-w-md p-8 bg-white shadow-md rounded-lg ">
-        <h1 className="text-2xl font-bold mb-6 text-center">Select Role</h1>
-        <div className="flex justify-between">
+    <div className="flex items-center justify-center min-h-screen bg-blue-900">
+      <div className="bg-white rounded-lg shadow-lg p-10 w-80">
+        <h1 className="text-2xl font-bold text-center mb-6">
+          Welcome to SkillStream
+        </h1>
+        <img src={welcome} alt="Welcome" className="mb-5"/>
+        <div className="flex flex-col space-y-4 items-center">
           <Link to="/teacher-login">
-            <button className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">
-              Teacher
-            </button>
+          <button className="bg-blue-500 text-white py-3 w-60 rounded-md hover:bg-blue-600 transition">
+            Teacher
+          </button>
           </Link>
           <Link to="/student-login">
-            <button className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">
-              Student
-            </button>
+          <button className="bg-blue-500 text-white py-3 w-60 rounded-md hover:bg-blue-600 transition">
+            Student
+          </button>
           </Link>
         </div>
       </div>
