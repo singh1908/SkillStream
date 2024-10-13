5b63af187c1ec2b47c8e17a0c125ebeae87ffb32
Vivek Kumar Singh <vivekkumarsingh.mail04@gmail.com>
2024-10-13 12:11:58 +0530

    Total score added

--- frontend/src/StudentPages/Header.jsx
+++ frontend/src/StudentPages/Header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
         </div>
         <div className="flex items-center">
           {/* Placeholder for Image */}
-          <div className="w-10 h-10 bg-gray-300 rounded-full mr-4"></div>
+          {/* <div className="w-10 h-10 bg-gray-300 rounded-full mr-4"></div> */}
           <div>
             <p className="text-gray-500">Today,</p>
             <p className="text-xl font-bold"><CurrentDate /></p>
--- frontend/src/StudentPages/Student.jsx
+++ frontend/src/StudentPages/Student.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState, useRef } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { useCamera } from "./CameraContext";
+import { Link } from "react-router-dom";
 
 function Student() {
   const { id } = useParams();
@@ -73,7 +74,10 @@ function Student() {
     <div className="max-w-3xl mx-auto p-4 bg-white shadow-md rounded-lg relative">
       {showScore ? (
         <div className="text-center">
-          <h1 className="text-3xl font-bold mb-4">Your Score: {score}</h1>
+          <h1 className="text-3xl font-bold mb-4">Your Score: {score} / {quiz.questions.length} </h1>
+          <Link to="/attempt-quiz">
+            <button className="bg-blue-500 text-white p-2 rounded">Back</button>
+          </Link>
         </div>
       ) : (
         <div>
